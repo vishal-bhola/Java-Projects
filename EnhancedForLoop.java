9a0726a2f1373f07a1d9ea9cd4f0633ccf4483e9
Vishal <38592928+VersatileVishal@users.noreply.github.com>
2019-01-14 19:07:12 +0530

    Update EnhancedForLoop.java

--- EnhancedForLoop.java
+++ EnhancedForLoop.java
@@ -2,8 +2,8 @@ public class EnhancedForLoop
 {
 public static void main(String[]args)
  {
-  int [] numbers={10,20,30,40,50};
-  for(int i:numbers)
+   int [] numbers={10,20,30,40,50};
+   for(int i:numbers)
     System.out.println("i: "+i);
  }
 }
