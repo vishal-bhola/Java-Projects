25daeb92209a376a6a541827b3d4cbeee8cb1ac4
Vishal <38592928+VersatileVishal@users.noreply.github.com>
2019-01-14 19:03:41 +0530

    Add files via upload

---
+++ SumByTakingInputFromCmdPrompt.java
@@ -0,0 +1,9 @@
+public class SumByTakingInputFromCmdPrompt
+{
+ public static void main(String[] args)
+{
+ int a=Integer.parseInt(args[0]);
+ int b=Integer.parseInt(args[1]);
+ System.out.println("Sum using cmd prompt is "+(a+b));
+}
+}
---
+++ TotalInputs.java
@@ -0,0 +1,7 @@
+public class TotalInputs
+{
+public static void main(String[] args)
+{ 
+  System.out.println("Total Inuts by users are: "+args.length);
+}
+}
