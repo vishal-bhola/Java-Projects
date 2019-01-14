fb6318819068071def67e006d393a7b6fbc5c930
Vishal <38592928+VersatileVishal@users.noreply.github.com>
2019-01-14 19:13:33 +0530

    Update TwoDimensionalArray.java

--- TwoDimensionalArray.java
+++ TwoDimensionalArray.java
@@ -1,20 +1,21 @@
 public class TwoDimensionalArray
 {
-   public static void main(String [] args)
+  public static void main(String [] args)
    { 
      int [][]x=new int [3][];
 
-     x[0]=new int[]{0,1,2,3};
-     x[1]=new int[]{0,1,2};
-     x[2]=new int[]{0,1,2,3,4};
+      x[0]=new int[]{0,1,2,3};
+      x[1]=new int[]{0,1,2};
+      x[2]=new int[]{0,1,2,3,4};
     
      for(int i=0;i<x.length;i++)
      {
-      for(int j=0;j<x[i].length;j++)
-       {
-         System.out.print(x[i][j]);
-       }    
-        System.out.print("\n");
+       for(int j=0;j<x[i].length;j++)
+        {
+          System.out.print(x[i][j]);
+        }    
+          System.out.print("\n");
      } 
    }
+   
 }
