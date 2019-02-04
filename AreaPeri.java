ba579bb460d88b6c2098c812394a8fd9d69284d9
Vishal Bhola <38592928+Versatile-Vishal@users.noreply.github.com>
2019-02-05 00:08:33 +0530

    Add files via upload

---
+++ AreaPeri.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+
+public class AreaPeri 
+{
+
+    public static void main(String args[]) 
+ 
+   {
+
+         Scanner sc=new Scanner(System.in);
+       
+         int l=sc.nextInt();
+
+         int b=sc.nextInt();
+
+         
+ int area=l*b;
+
+          int peri=2*(l+b);
+  
+       
+ if(area>peri)
+
+         {
+
+             System.out.println("Area");
+
+             System.out.println(area);
+ 
+         }
+         
+    
+       else if(peri>area)
+ 
+        {
+   
+          System.out.println("Peri");
+
+          System.out.println(peri);
+ 
+        }
+   
+     else
+
+         {
+   
+          System.out.println("Eq");
+   
+          System.out.println(peri);
+
+         }
+ 
+    }
+
+}
