bdf46db38438baa45dab832e6118c84f8e4e08bb
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2019-03-27 20:22:49 +0530

    Create OperatorDemo.java

---
+++ OperatorDemo.java
@@ -0,0 +1,38 @@
+
+public class OperatorDemo {
+
+	/*
+	 * 
+	 * Arithmetic  +,-,*,/,%
+	 * Bitwise     
+	 * Relational 
+	 * Logical
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) 
+	{
+//		int m=6,n=4;
+//		int r1=m+n;
+//		int r2=m-n;
+//		int r3=m*n;
+//		double r4= (double) m/n;
+//		int r5 = m%n;
+//		
+//		System.out.println(r1);
+//		System.out.println(r2);
+//		System.out.println(r3);
+//		System.out.println(r4);
+//		System.out.println(r5);
+		
+		int m=4;
+		int n=5;
+		
+		n+=m; // n = n + m
+		n++; // n=n+1
+		
+		++n;
+		System.out.println(n);
+	}
+
+}
